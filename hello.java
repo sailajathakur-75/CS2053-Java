5f1a41edd4b8220d8690004e9059b7247d73df21
sailajathakur-75 <sailajathakur@gmail.com>
2025-10-16 06:14:24 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ class hello{
     public static void main(String args[]){
         System.out.println("Hello, World!");
         System.out.println("This is my first Hello, World program in Java.");
+        System.out.println("This is my first change in hello world.");
+        
     }       
 }
